dc99377ef7844ac626cfad025effaae0249baff6
TsubasaKomiyama <kamakuraaaabakuhu@yahoo.co.jp>
2021-06-26 10:54:24 +0900

    Update HandleCommand.js

--- HandleCommand.js
+++ HandleCommand.js
@@ -1,7 +1,10 @@
 const discord = require('discord.js');
-const prefix = '/'; // prefix
+const prefix = '/';
 
-// コマンド一覧
+/**
+ * メニュー一覧
+ * @module exports
+ */
 module.exports = {
 	call: call,
 	isValidLobby: isValidLobby,
@@ -14,38 +17,48 @@ function call (message) {
 	const args = message.content.slice(prefix.length).split(' ');
 	const command = args.shift().toLowerCase();
 
-	switch (command){
-	// lobby
+	switch (command)　{
+
 	case 'lobby':
 		lobby (message, args[0]);
 		break;
-	// rename
+
 	case 'rename':
 		rename (message, args[0]);
 		break;
 	}
 }
 
-// /lobbyが有効なコマンドか確認を行う
+/**
+ * 作成可能なチャンネルか確認する。
+ * @function
+ */
 function isValidLobby (message, lobby) {
-	// 利用出来るチャンネルを指定する場合
+	/**
+	 * // TODO
+	 * 規定のチャンネルでのみ実行出来る。
+	 * 既存カテゴリと重複する名前は受け付けない。
+	 * 有効ならばlobbyの処理を続行する。
+	 */
+
+	// 規定のチャンネル以外で入力
 	if (!(message.channel.id === 'チャンネルID')) {
 		message.reply('このチャンネルでは実行出来ません。');
 		return false;
 	}
 
-	// lobbyがない
+	// args[0]がない
 	if (!(lobby)) {
 		message.reply('作成するロビー名を入力してください。');
 		return false;
 	}
 
+	/** 検索するカテゴリ名 lobbyName */
 	const lobbyName = lobby;
+	// カテゴリー検索
+	const searchCategory = message.guild.channels.find ((category) => category.name.toLowerCase() === lobbyName && category.type == 'category');
 
-	// lobbyNameとカテゴリー名が一致するカテゴリー検索
-	const searchCategory = message.guild.channels.find((category) => category.name.toLowerCase() === lobbyName.toLowerCase() && category.type == 'category');
-
-	// lobbyNameと同名の既存カテゴリーがある
+	// args[0]と同名の既存カテゴリーがある
 	if (searchCategory) {
 		message.reply('既に同一名称のカテゴリーが存在します。');
 		return false;
@@ -54,16 +67,21 @@ function isValidLobby (message, lobby) {
 	return true; // trueで返す
 }
 
-// /renameが有効なコマンドか確認を行う
+/**
+ * 変更可能なチャンネルか確認する。
+ * @function
+ */
 function isValidRename (message, lobby) {
-	// 変更されたくないカテゴリーを指定する場合
-	if (message.channel.id === 'チャンネルID1' || message.channel.id === 'チャンネルID2') {
-		message.reply('このカテゴリー名称は変更出来ません。');
-		return false;
-	}
-	// messageを分けたい場合
-	if (message.channel.id === 'チャンネルID3') {
-		message.reply('このカテゴリー名称を変更してはいけません。');
+	/**
+	 * // TODO
+	 * 常設カテゴリ以外で実行出来る。
+	 * 既存カテゴリと重複する名前は受け付けない。
+	 * 有効ならばrenameの処理を続行する。
+	 */
+
+	// 常設チャンネルで入力
+	if (message.channel.id === 'チャンネルID') {
+		message.reply('このカテゴリーは変更出来ません。');
 		return false;
 	}
 
@@ -73,18 +91,18 @@ function isValidRename (message, lobby) {
 		return false;
 	}
 
-	// lobbyがない
+	// args[0]がない
 	if (!(lobby)) {
 		message.reply('作成するロビー名を入力してください。');
 		return false;
 	}
 
+	/** 検索するカテゴリ名 lobbyRename */
 	const lobbyRename = lobby;
+	// カテゴリー検索
+	const searchCategory = message.guild.channels.find ((category) => category.name.toLowerCase() === lobbyRename && category.type == 'category')
 
-	// lobbyRenameとカテゴリー名が一致するカテゴリー検索
-	const searchCategory = message.guild.channels.find((category) => category.name.toLowerCase() === lobbyRename.toLowerCase() && category.type == 'category')
-
-	// lobbyRenameと同名の既存カテゴリーがある
+	// args[0]と同名の既存カテゴリーがある
 	if (searchCategory) {
 		message.reply('既に同一名称のカテゴリーが存在します。');
 		return false;
@@ -93,46 +111,64 @@ function isValidRename (message, lobby) {
 	return true; // trueで返す
 }
 
-// ロビー作成
+/**
+ * 入力された値を受け取り、カテゴリとチャンネルを作成する。
+ * @function
+ */
 function lobby (message, lobby) {
-	if (!(isValidLobby(message, lobby))) {return; } // 非有効なコマンド
+	/**
+	 * //TODO
+	 * カテゴリを作成する。
+	 * 予め名称の決まっているテキストチャンネルとボイスチャンネルを作成する。
+	 * それらを作成したカテゴリに配置する。
+	 */
 
+	if (!(isValidLobby(message, lobby))) {return; } // 非有効なコマンドはreturn
+
+	/** 作成するカテゴリ名 lobbyName */
 	const lobbyName = lobby;
+	/** 作成するテキストチャンネル名  txt */
 	const txt = 'テキストチャンネル';
+	/** 作成するボイスチャンネル名 vc*/
 	const vc = 'ボイスチャンネル';
 
 	// カテゴリー作成
 	message.guild.createChannel(lobbyName, {type: 'category'})
-	.then(console.log("Created：" + lobbyName))
-	.catch(console.error);
+	.then (console.log("Created：" + lobbyName))
+	.catch (console.error);
 
 	// テキストチャンネル作成
 	message.guild.createChannel(txt, {type: 'text'})
-	.then((channel) => {
-		// lobbyNameとカテゴリー名が一致するカテゴリー検索
-		let searchCategory = message.guild.channels.find((category) => category.name == lobbyName && category.type == 'category');
-		if (!(category)) { return; }
+	.then ((channel) => {
+		// lobbyNameと一致するカテゴリー検索
+		let searchCategory = message.guild.channels.find ((category) => category.name == lobbyName && category.type == 'category');
+		if (!(searchCategory)) { return; }
 		channel.setParent(category.id); // カテゴリーに配置
-	}).catch(console.error);
+	}).catch (console.error);
 
 	// ボイスチャンネル作成
 	message.guild.createChannel(vc, {type: 'voice', userLimit: 99})
 	.then((channel) => {
-		// lobbyNameとカテゴリー名が一致するカテゴリー検索
-		let searchCategory = message.guild.channels.find((category) => category.name == lobbyName && category.type == 'category');
-		if (!(category)) { return; }
+		// lobbyNameと一致するカテゴリー検索
+		let searchCategory = message.guild.channels.find ((category) => category.name == lobbyName && category.type == 'category');
+		if (!(searchCategory)) { return; }
 		channel.setParent(category.id); // カテゴリーに配置
 	}).catch(console.error);
 }
 
-// ロビー名編集
+/**
+ * 作成したカテゴリのカテゴリ名を変更する。
+ * @function
+*/
 function rename (message, lobby) {
-	if (!(isValidRename(message, lobby))) {return; } // 非有効なコマンド
+	if (!(isValidRename(message, lobby))) {return; } // 非有効なコマンドはreturn
 
+	/** 変更後のカテゴリ名 lobbyRename*/
 	const lobbyRename = lobby;
 
-	// カテゴリチャンネル名にrenameLobbyを代入
+	// カテゴリ名をlobbyRenameに変更
+	// 仕様上、テキストチャンネルがないカテゴリの名称は変更出来ない。
 	message.channel.parent.setName(lobbyRename)
-	.then(console.log('Edited：' + lobbyRename))
-	.catch(console.error);
+	.then (console.log('Edited：' + lobbyRename))
+	.catch (console.error);
 }
